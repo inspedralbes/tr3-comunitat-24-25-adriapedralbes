cf73bfd73ee06c131acafbe5960f6f018fb09978
adriapedralbes <a23adrestmar@inspedralbes.cat>
2025-03-11 20:04:56 +0100

    Feat: Añadir configuración para permitir solicitudes desde randomuser.me en next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -47,6 +47,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        port: '',
+        pathname: '/**',
+      },
     ],
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
