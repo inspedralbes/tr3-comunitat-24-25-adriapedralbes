6f8ae7a98d37bb3e1ddb718e93037fbfbb46332c
adriapedralbes <a23adrestmar@inspedralbes.cat>
2025-03-11 20:26:49 +0100

    Refactor: Eliminar scripts y archivos obsoletos relacionados con la creación de superusuarios y configuración de Docker

--- create_admin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Ejecutar el script Python dentro del contenedor de Django
-docker exec -it django-app python /app/create_superuser.py
-
-echo "¡Proceso completado! Ahora deberías poder iniciar sesión con usuario 'admin' y contraseña 'admin123'"
