42e3962c9b57324d55d1dfef7b23bf0f18449719
adriapedralbes <a23adrestmar@inspedralbes>
2025-03-28 10:16:26 +0100

    Add media API configuration to Next.js settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -47,6 +47,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.futurprive.com',
+        port: '',
+        pathname: '/media/**',
+      }
     ],
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
